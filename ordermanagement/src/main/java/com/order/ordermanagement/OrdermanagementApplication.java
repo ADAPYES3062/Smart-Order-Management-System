3f6db97d1564cdc35aa0a2c2ade6610df90ba386
Christan Wilbert <chriwilbert.ext@deloitte.com>
2025-11-04 14:36:21 +0530

    Configuration Server Update

--- ordermanagement/src/main/java/com/order/ordermanagement/OrdermanagementApplication.java
+++ ordermanagement/src/main/java/com/order/ordermanagement/OrdermanagementApplication.java
@@ -3,17 +3,11 @@ package com.order.ordermanagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 @SpringBootApplication
 public class OrdermanagementApplication {
 
 	public static void main(String[] args) {
 
-		Dotenv dotenv = Dotenv.load();
-        System.setProperty("DB_URL", dotenv.get("DB_URL"));
-        System.setProperty("DB_USER", dotenv.get("DB_USER"));
-        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 		SpringApplication.run(OrdermanagementApplication.class, args);
 	}
 
