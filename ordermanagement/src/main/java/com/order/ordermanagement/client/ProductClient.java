5818ab0b0f8aece0383aeebd6a99473dc0fa9e7b
Yeswanth <adapyeswanth.ext@deloitte.com>
2025-11-10 14:38:53 +0530

    Order summation

--- ordermanagement/src/main/java/com/order/ordermanagement/client/ProductClient.java
+++ ordermanagement/src/main/java/com/order/ordermanagement/client/ProductClient.java
@@ -3,12 +3,21 @@ package com.order.ordermanagement.client;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "product-service", url = "http://localhost:9292")
+import com.order.ordermanagement.model.ApiResponse;
+import com.order.ordermanagement.model.ProductResponse;
+
+@FeignClient(name = "product-service", url = "http://localhost:9393")
 public interface ProductClient {
 
     @GetMapping("/products/{id}")
-    String getProductById(@PathVariable long id);
+    ApiResponse<ProductResponse> getProductById(@PathVariable long id);
+
+    @PutMapping("/products/{id}/reduce")
+    ApiResponse<String> reduceProductQuantity(@PathVariable Long id, @RequestParam int quantity);
+
     
     
 }
