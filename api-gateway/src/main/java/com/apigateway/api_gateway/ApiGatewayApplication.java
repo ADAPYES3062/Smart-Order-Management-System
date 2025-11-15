e7b7bc8ee3472d5e77d56b12783f0685e4f4988f
Christan Wilbert <chriwilbert.ext@deloitte.com>
2025-11-15 17:54:31 +0530

    feat: Api Gateway

--- api-gateway/src/main/java/com/apigateway/api_gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/apigateway/api_gateway/ApiGatewayApplication.java
@@ -1,18 +1,15 @@
-package com.order.ordermanagement;
+package com.apigateway.api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
 @EnableDiscoveryClient
-public class OrdermanagementApplication {
+public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
-
-		SpringApplication.run(OrdermanagementApplication.class, args);
+		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
 }
